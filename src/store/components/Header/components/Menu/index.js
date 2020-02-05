921075c5e3f3e7feb6cf0af1d55631ec148e3d03
Deyvison Rezende <deyvison.silva@corebiz.com.br>
2020-02-05 08:55:54 -0300

    estilização do header

--- src/store/components/Header/components/Menu/index.js
+++ src/store/components/Header/components/Menu/index.js
@@ -1,3 +1,27 @@
 import "./index.scss";
 
-export const Menu = () => {};
+export const Menu = () => { };
+
+$(document).ready(function () {
+	$(".menu-icon").click(function () {
+		$("ul.menu-ul").addClass("is-active--menu");
+	});
+});
+
+$(document).ready(function () {
+	$(".menu-icon").click(function () {
+		$("#backdrop-minicart").addClass("is-active--backdrop");
+	});
+});
+
+$(document).ready(function () {
+	$("#backdrop-minicart").click(function () {
+		$("ul.menu-ul").removeClass("is-active--menu");
+	});
+});
+
+$(document).ready(function () {
+	$("#backdrop-minicart").click(function () {
+		$("#backdrop-minicart").removeClass("is-active--backdrop");
+	});
+});
--- src/store/components/Header/components/Minicart/index.js
+++ src/store/components/Header/components/Minicart/index.js
@@ -1,3 +1,27 @@
 import "./index.scss";
 
-export const MiniCart = () => {};
+export const MiniCart = () => { };
+
+$(document).ready(function () {
+	$(".cart--icon-link").click(function () {
+		$(".v2-vtexsc-cart").addClass("is-active--minicart");
+	});
+});
+
+$(document).ready(function () {
+	$(".cart--icon-link").click(function () {
+		$("#backdrop-minicart").addClass("is-active--backdrop");
+	});
+});
+
+$(document).ready(function () {
+	$("#backdrop-minicart").click(function () {
+		$(".v2-vtexsc-cart").removeClass("is-active--minicart");
+	});
+});
+
+$(document).ready(function () {
+	$("#backdrop-minicart").click(function () {
+		$("#backdrop-minicart").removeClass("is-active--backdrop");
+	});
+});
